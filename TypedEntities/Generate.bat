95e6de04b3d66e082ee780fb35d345fa46dc7a0b
admin <jordi.montana@gmail.com>
2017-10-05 00:15:35 +0200

    Added typedentities

---
+++ TypedEntities/Generate.bat
@@ -0,0 +1,20 @@
+@ECHO OFF
+ECHO Generating entities...
+
+..\sdk\Bin\CrmSvcUtil.exe ^
+/codewriterfilter:"CrmSvcUtilExtensions.BasicFilteringService, CrmSvcUtilExtensions" ^
+/url:https://directionsemeatesting.crm4.dynamics.com/XRMServices/2011/Organization.svc ^
+/username:jordi@directionsemeatesting.onmicrosoft.com /password:DirectionsEmea17 ^
+/out:GeneratedCode.cs
+
+ECHO Generating option sets...
+..\sdk\Bin\CrmSvcUtil.exe ^
+/codewriterfilter:"Microsoft.Crm.Sdk.Samples.FilteringService, GeneratePicklistEnums" ^
+/codecustomization:"Microsoft.Crm.Sdk.Samples.CodeCustomizationService, GeneratePicklistEnums" ^
+/namingservice:"Microsoft.Crm.Sdk.Samples.NamingService, GeneratePicklistEnums" ^
+/url:https://directionsemeatesting.crm4.dynamics.com/XRMServices/2011/Organization.svc ^
+/username:jordi@directionsemeatesting.onmicrosoft.com /password:DirectionsEmea17 ^
+/out:OptionSets.cs
+
+ECHO Done! :)
+pause
