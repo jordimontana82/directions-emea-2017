c0ac900e9b303dc3231893c36ffcce99b56e8a1e
admin <jordi.montana@gmail.com>
2017-10-05 05:11:56 +0200

    Added MVC Unit test templates

---
+++ DirectionsEmea.Web.Tests/Commands/CommandSaveVoteProcessTests.cs
@@ -0,0 +1,35 @@
+﻿using DirectionsEmea.Web.Commands;
+using FakeXrmEasy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using TypedEntities;
+
+namespace DirectionsEmea.Web.Tests.Commands
+{
+    public class CommandSaveVoteProcessTests
+    {
+        [Fact]
+        public void When_badge_is_not_valid_vote_isnt_created()
+        {
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void When_already_voted_vote_isnt_created()
+        {
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void When_valid_vote_is_created()
+        {
+            Assert.True(false);
+        }
+
+
+    }
+}
